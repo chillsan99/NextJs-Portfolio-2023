9e53fec0321841e5b217b2f336b156391f446b96
chillsan99 <chilliary17023@gmail.com>
2023-03-15 12:53:08 -0400

    Website Changes

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { projectData } from '@/pages/data'
+import {AiFillGithub} from 'react-icons/ai'
+
+const Card = ({setDarkMode}) => {
+  return (
+    <>
+        <div className='m-auto flex flex-col gap-10 lg:flex-row lg:flex-wrap w-3/4'>
+        {projectData.map((data, key) =>{
+            return(
+                    <div key={key} className= "dark:bg-gray-800 drop-shadow-lg border-2 rounded-2xl p-10 basis-1/3 flex-1 h-90">
+                        <img className="rounded-2xl w-full h-44 object-cover drop-shadow-xl dark:text-white" src={data.image} alt={data.pr_name} width={'100%'} height={'100%'} layout="responsive"></img>
+                        <h4 className='text-xl pt-5 text-center font-medium py-2  dark:text-white'>{data.pr_name}</h4>
+                        <div>
+                            <p className='pb-2 text-md font-medium text-center  dark:text-white'>{data.lang_used}</p>
+                            <p className='leading-8 sm:text-small pb-4 dark:text-white flex-1'>{data.about}</p>
+                        <div className='flex justify-between relative flex-1'>
+                            <a className='bg-cyan-400 p-3 rounded-2xl dark:text-white inset-x-0 bottom-0' 
+                                href={data.projdemo_link}>Project Link</a>
+                            <a className='bg-cyan-600 p-3 rounded-2xl dark:text-white' href={data.projgithub_link}><span className='flex flex-row '><AiFillGithub className='text-2xl mr-2'/>GitHub</span></a>
+                        </div>
+                    </div>
+                </div>
+            )
+        })}
+        </div>
+    </>
+  )
+}
+
+export default Card
